217e3e2da2a5b8b563a7d202efdbc1db51af9100
Tan <22130246@st.hcmuaf.edu.vn>
2025-05-17 19:24:22 +0700

    Cap nhat cau lenh sql them san pham vao gio hang

--- src/main/java/SeafoodShop/dao/DataConnect.java
+++ src/main/java/SeafoodShop/dao/DataConnect.java
@@ -25,6 +25,19 @@ public class DataConnect {
         return conn;
     }
 
+    public int getUserID(String username, String password) throws SQLException {
+        getConnection();
+        String sql = "select * from Users where Username=? and password=?";
+        PreparedStatement ps =  conn.prepareStatement(sql);
+        ps.setString(1, username);
+        ps.setString(2, password);
+        ResultSet rs = ps.executeQuery();
+        if(rs.next()){
+            return rs.getInt("UserID");
+        }
+        return -1;
+    }
+
     public int getUserRole(String username, String password) throws SQLException {
         getConnection();
         String sql = "select * from Users where Username=? and password=?";
@@ -87,13 +100,64 @@ public class DataConnect {
         return pr;
     }
 
+    public int getProductCount(int product_id) throws SQLException {
+        getConnection();
+        String sql = "SELECT \n" +
+                "    p.ProductID,\n" +
+                "    p.Name,\n" +
+                "    ISNULL(SUM(pid.Quantity), 0) AS TotalImported,\n" +
+                "    ISNULL(SUM(od.Quantity), 0) AS TotalSold,\n" +
+                "    (ISNULL(SUM(pid.Quantity), 0) - ISNULL(SUM(od.Quantity), 0)) AS StockQuantity\n" +
+                "FROM \n" +
+                "    Products p\n" +
+                "LEFT JOIN (\n" +
+                "    SELECT \n" +
+                "        pid.ProductID, \n" +
+                "        SUM(pid.Quantity) AS Quantity\n" +
+                "    FROM \n" +
+                "        ProductInDetail pid\n" +
+                "    JOIN ProductIn pi ON pid.ProductInID = pi.ProductInID\n" +
+                "    GROUP BY pid.ProductID\n" +
+                ") pid ON p.ProductID = pid.ProductID\n" +
+                "LEFT JOIN (\n" +
+                "    SELECT \n" +
+                "        od.ProductID, \n" +
+                "        SUM(od.Quantity) AS Quantity\n" +
+                "    FROM \n" +
+                "        OrderDetails od\n" +
+                "    JOIN Orders o ON od.OrderID = o.OrderID\n" +
+                "    GROUP BY od.ProductID\n" +
+                ") od ON p.ProductID = od.ProductID\n" +
+                "WHERE p.ProductID = ? \n" +
+                "GROUP BY \n" +
+                "    p.ProductID, p.Name;\n";
+        PreparedStatement ps =  conn.prepareStatement(sql);
+        ps.setInt(1, product_id);
+        ResultSet rs = ps.executeQuery();
+        if(rs.next()){
+            return rs.getInt("StockQuantity");
+        }
+        return -1;
+    }
 
+    public void addProductToCart(int user_id,int product_id) throws SQLException {
+        getProductByID(product_id);
+        if(getProductCount(product_id) - 1 >= 0){
+            String sql = "INSERT INTO [dbo].[Cart] ([UserID],[ProductID],[Quantity],[AddedAt])\n" +
+                    "VALUES (? , ?, 1, GETDATE());";
+            PreparedStatement ps =  conn.prepareStatement(sql);
+            ps.setInt(1, user_id);
+            ps.setInt(2, user_id);
+            ps.executeUpdate();
+        }
+    }
 
     public static void main(String[] args) throws SQLException {
         DataConnect dc = new DataConnect();
         dc.getConnection();
 //        System.out.println(dc.getUserRole("nguyenvana","123456"));
 //        System.out.println(dc.getProductList());
-        System.out.println(dc.getProductByID(2));
+//        System.out.println(dc.getProductByID(2));
+        System.out.println(dc.getProductCount(1));
     }
 }
